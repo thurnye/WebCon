7bd6e720155af5f73da92df9c9739e14d084d75e
Daniel Tamunotonye <dan_tonye@Gudi.local>
2023-08-05 00:41:23 -0400

    setup client

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+
+function App() {
+
+  return (
+    <React.Fragment>
+      welcome to react
+    </React.Fragment>
+  );
+}
+export default App;
